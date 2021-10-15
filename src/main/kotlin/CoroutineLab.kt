9502391cd62c05bb70e307d07a64e13bb0bfea0a
root <root@DESKTOP-5NPG1UR.localdomain>
2021-10-15 15:28:15 +0000

    TLC Day 3 Kotlin

--- src/main/kotlin/CoroutineLab.kt
+++ src/main/kotlin/CoroutineLab.kt
@@ -1,4 +1,4 @@
-import kotlinx.coroutines.*
+//import kotlinx.coroutines.*
 
 // Question 1
 //suspend fun main()= coroutineScope{
@@ -26,45 +26,45 @@ import kotlinx.coroutines.*
 
 ////Question 3
 //
-//import kotlinx.coroutines.*
-//import kotlinx.coroutines.channels.Channel
-//import kotlin.random.Random as kotlinRandom
-//
-//suspend fun main()= coroutineScope{
-//    val primeNumberList = sieveOfEratosthenes(100)
-//    println("List of prime numbers less than 101")
-//    println(primeNumberList)
-//    val mq = Channel<Int>()
-//    launch {
-//        while (true){
-//            for (m in mq){
-//                if(primeNumberList.contains(m))
-//                    println("$m is prime")
-//                else
-//                    println("$m is not prime")
-//            }
-//        }
-//    }
-//    for (i in 1..10){
-//        var random = kotlinRandom.nextInt(2,100)
-//        mq.send(random)
-//        delay(2000)
-//    }
-//    mq.close()
-//    println("All done")
-//
-//}
-//
-//fun sieveOfEratosthenes(number: Int): List<Int>{
-//    var prepend = mutableListOf(2,3,5)
-//    var list = mutableListOf<Int>()
-//    for (i in 6..number)
-//        list.add(i)
-//
-//    var newList = list.stream().filter{it % 2 != 0 && it % 3 != 0 && it % 5 != 0}.toList()
-//
-//    val primeList = prepend + newList
-//
-//    return primeList
-//}
-//
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlin.random.Random as kotlinRandom
+
+suspend fun main()= coroutineScope{
+    val primeNumberList = sieveOfEratosthenes(100)
+    println("List of prime numbers less than 101")
+    println(primeNumberList)
+    val mq = Channel<Int>()
+    launch {
+        while (true){
+            for (m in mq){
+                if(primeNumberList.contains(m))
+                    println("$m is prime")
+                else
+                    println("$m is not prime")
+            }
+        }
+    }
+    for (i in 1..10){
+        var random = kotlinRandom.nextInt(2,100)
+        mq.send(random)
+        delay(2000)
+    }
+    mq.close()
+    println("All done")
+
+}
+
+fun sieveOfEratosthenes(number: Int): List<Int>{
+    var prepend = mutableListOf(2,3,5)
+    var list = mutableListOf<Int>()
+    for (i in 6..number)
+        list.add(i)
+
+    var newList = list.stream().filter{it % 2 != 0 && it % 3 != 0 && it % 5 != 0}.toList()
+
+    val primeList = prepend + newList
+
+    return primeList
+}
+
